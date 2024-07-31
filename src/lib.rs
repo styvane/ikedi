81ecaf6cf3d5ba2c68a4f08099df1b7af263aa91
Sede Soukossi <4968379+styvane@users.noreply.github.com>
2024-07-31 09:02:58 +0200

    feat: Parse DICOM files

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,30 @@ pub mod command;
 pub mod error;
 pub mod parser;
 
+use std::path::{Path, PathBuf};
+
+use self::error::Result;
+use self::parser::DicomData;
+
 /// DicomProcessor defines the various operations that
-/// can be performed on DICOM files.
-pub trait DicomProcessor {}
+/// can be performed on DICOM files
+pub trait DicomFileProcessor {
+    /// Catalog root path.
+    type CatalogPath;
+    /// Creates a DIDCOM data from the DICOM file.
+    fn open(&self) -> Result<DicomData<Self::CatalogPath>>;
+}
+
+impl DicomFileProcessor for Path {
+    type CatalogPath = PathBuf;
+
+    fn open(&self) -> Result<DicomData<PathBuf>> {
+        let file_obj = dicom_object::open_file(self)?;
+        let patient = file_obj.try_into()?;
+        let data = DicomData::new(self.to_path_buf(), patient);
+        Ok(data)
+    }
+}
+
+#[cfg(test)]
+mod tests {}
--- src/parser.rs
+++ src/parser.rs
@@ -1,14 +1,65 @@
-//! This module implement the parsing logic for DICOM files
+//! This module implements the parsing logic for DICOM files
 
+use std::error::Error;
 use std::path::Path;
 
-use walkdir::DirEntry;
-use walkdir::WalkDir;
+use dicom_object::{FileDicomObject, InMemDicomObject};
+use walkdir::{DirEntry, WalkDir};
+
+use crate::error::Result;
 
 /// Recursively read the directory.
-pub fn read_directory(path: impl AsRef<Path>) -> impl Iterator<Item = DirEntry> {
-    WalkDir::new(path)
+pub fn read_directory(source: impl AsRef<Path>) -> impl Iterator<Item = DirEntry> {
+    WalkDir::new(source)
         .into_iter()
         .filter_map(|entry| entry.ok())
         .filter(|entry| entry.file_type().is_file())
 }
+
+/// DICOM Data type.
+pub struct DicomData<P> {
+    /// Absolute path to the DICOM data in the catolog.
+    path: P,
+    /// Patient data
+    pub patient: Patient,
+}
+
+/// Patient represents the patient data.
+pub struct Patient {
+    /// The patient name
+    pub name: String,
+    /// The patient ID
+    pub id: String,
+}
+
+impl TryFrom<FileDicomObject<InMemDicomObject>> for Patient {
+    type Error = Box<dyn Error>;
+    fn try_from(file_obj: FileDicomObject<InMemDicomObject>) -> Result<Self> {
+        let name = file_obj
+            .element_by_name("PATIENT_NAME")?
+            .to_str()?
+            .to_string();
+        let id = file_obj
+            .element_by_name("PATIENT_ID")?
+            .to_str()?
+            .to_string();
+        Ok(Self { name, id })
+    }
+}
+impl<P> DicomData<P>
+where
+    P: AsRef<Path>,
+{
+    /// Returns the path to the DICOM data in the catalog
+    pub fn path(&self) -> impl AsRef<Path> + '_ {
+        &self.path
+    }
+
+    /// Creates new [`DicomData`]
+    pub const fn new(path: P, patient: Patient) -> Self {
+        Self { path, patient }
+    }
+}
+
+#[cfg(test)]
+mod tests {}
